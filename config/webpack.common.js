484374acb32ac90858a976e25e62b7b9332c842a
Val Neekman <val@neekware.com>
2016-06-22 20:45:58 -0400

    look and feel change

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -18,7 +18,7 @@ const HtmlElementsPlugin = require('./html-elements-plugin');
  * Webpack Constants
  */
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'xChange Stock Market Portal',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
