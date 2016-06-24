1db6d232b8085015e19dc43fa288a58a99978c77
Val Neekman <val@neekware.com>
2016-06-23 20:43:57 -0400

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { RouterConfig } from '@angular/router';
 import { HomeComponent } from './home';
-import { NoContent } from './no-content';
+import { UnfoundComponent } from './unfound';
 
 export const routes: RouterConfig = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
   // make sure you match the component type string to the require in asyncRoutes
   { path: 'about', component: 'AboutComponent' },
-  { path: '**',    component: NoContent },
+  { path: '**',    component: UnfoundComponent },
 ];
 
 // Async load a component using Webpack's require with es6-promise-loader and webpack `require`
@@ -23,6 +23,5 @@ export const asyncRoutes: AsyncRoutes = {
 export const prefetchRouteCallbacks: Array<Es6PromiseLoader | Function> = [
   asyncRoutes['AboutComponent'] // es6-promise-loader returns a function
 ];
-
 
 // Es6PromiseLoader and AsyncRoutes interfaces are defined in custom-typings
---
+++ src/app/unfound/unfound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'unfound',
+  styleUrls: ['./unfound.style.css'],
+  templateUrl: './unfound.template.html'
+})
+export class UnfoundComponent {
+
+}
